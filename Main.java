b803f347b22af46edc52b6d8c250deb8fc8b7781
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-08-30 17:26:50 +0800

    Updated Client landing page
    Added in required fonts

---
+++ Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Font.loadFont(Main.class.getResource("./CSS/Lato-Bold.ttf").toExternalForm(),20);
+        Font.loadFont(Main.class.getResource("./CSS/Lato-Medium.ttf").toExternalForm(),20);
+        Parent root = FXMLLoader.load(getClass().getResource("./View/ClientLanding.fxml"));
+        primaryStage.setTitle("DLSUsap");
+        primaryStage.setScene(new Scene(root, 1280 , 720));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
