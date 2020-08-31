1edc670016b59a9e8ac4df4b7acac98b096f3fca
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-08-31 14:29:32 +0800

    Moved classes into main Controller folder

--- Controller/Main.java
+++ Controller/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        Font.loadFont(Main.class.getResource("../CSS/Lato-Bold.ttf").toExternalForm(),20);
-        Font.loadFont(Main.class.getResource("../CSS/Lato-Medium.ttf").toExternalForm(),20);
-        Font.loadFont(Main.class.getResource("../CSS/Lato-Regular.ttf").toExternalForm(),20);
-        Parent root = FXMLLoader.load(getClass().getResource("../View/Client-Landing.fxml"));
+        Font.loadFont(Main.class.getResource("/CSS/Lato-Bold.ttf").toExternalForm(),20);
+        Font.loadFont(Main.class.getResource("/CSS/Lato-Medium.ttf").toExternalForm(),20);
+        Font.loadFont(Main.class.getResource("/CSS/Lato-Regular.ttf").toExternalForm(),20);
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Client-Landing.fxml"));
         primaryStage.setTitle("DLSUsap");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.setResizable(false);
