fbb2a2babc26b9b740fcad476137a502db150412
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-08-31 13:38:34 +0800

    Added initial ClientLandingController

--- Controller/Main.java
+++ Controller/Main.java
@@ -1,3 +1,5 @@
+package Controller; 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,8 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Font.loadFont(Main.class.getResource("./CSS/Lato-Bold.ttf").toExternalForm(),20);
-        Parent root = FXMLLoader.load(getClass().getResource("./View/Client-Interface.fxml"));
+        Font.loadFont(Main.class.getResource("../CSS/Lato-Bold.ttf").toExternalForm(),20);
+        Font.loadFont(Main.class.getResource("../CSS/Lato-Medium.ttf").toExternalForm(),20);
+        Font.loadFont(Main.class.getResource("../CSS/Lato-Regular.ttf").toExternalForm(),20);
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Client-Landing.fxml"));
         primaryStage.setTitle("DLSUsap");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.setResizable(false);
